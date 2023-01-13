758d1e0818b91a1e9112508cfa11686ee521200f
Jason Aucordier <jaucordi@gmail.com>
2023-01-13 17:02:10 +0100

    Cleaning
    - TS -> JS
    - Simplify App
    - Add sass main styles
    - Cleaning & Adding missing assets

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import Router from "./Router.jsx";
+
+function App() {
+    return (
+        <Router/>
+    )
+}
+
+export default App
---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import './styles/index.sass'
+import ReactDOM from 'react-dom/client'
+import React from 'react'
+import App from './App.jsx'
+
+const container = document.getElementById('root');
+const root = ReactDOM.createRoot(container);
+
+root.render(
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
+);
