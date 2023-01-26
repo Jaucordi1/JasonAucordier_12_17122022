ac595970e5cd0f963d04746d908a46f389e01822
Jason Aucordier <jaucordi@gmail.com>
2023-01-26 13:03:59 +0100

    Adding "Header" component

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,25 @@
+import Classes from "./Header.module.sass"
+import PropTypes from "prop-types";
+import {User} from "../../models/user/User.js";
+import classNames from "classnames";
+
+export const Header = ({user, className}) => {
+    return (
+        <section className={classNames(Classes.heading, className)}>
+            <h1 className={Classes.title}>
+                {"Bonjour "}
+                <span className={Classes.user}>
+                    {user.userInfos.firstName}
+                </span>
+            </h1>
+            <p className={Classes.subtitle}>
+                Félicitation ! Vous avez explosé vos objectifs hier 👏
+            </p>
+        </section>
+    );
+};
+
+Header.propTypes = {
+    user: PropTypes.instanceOf(User).isRequired,
+    className: PropTypes.string,
+};
