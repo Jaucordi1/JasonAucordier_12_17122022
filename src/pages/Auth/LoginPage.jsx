a7760d5701268df60b43cfc21e8cffe7d9db06b5
Jason Aucordier <jaucordi@gmail.com>
2023-01-26 13:07:34 +0100

    Adding a "temporary" login page for presentation

---
+++ src/pages/Auth/LoginPage.jsx
@@ -0,0 +1,20 @@
+import Classes from "./LoginPage.module.sass"
+import {NavLink} from "react-router-dom";
+
+const USERS = [12, 18];
+
+const LoginPage = () => {
+    return (
+        <main className={Classes.container}>
+            {USERS.map(userID => (
+                <NavLink key={userID} to={`/${userID}`} className={Classes.user}>
+                    {userID}
+                </NavLink>
+            ))}
+        </main>
+    );
+}
+
+LoginPage.propTypes = {};
+
+export default LoginPage;
