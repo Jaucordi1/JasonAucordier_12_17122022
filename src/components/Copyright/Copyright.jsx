7c25f8016be3292bebcf8848d809e1cf17d3b448
Jason Aucordier <jaucordi@gmail.com>
2023-01-26 13:03:23 +0100

    Adding "Copyright" component

---
+++ src/components/Copyright/Copyright.jsx
@@ -0,0 +1,9 @@
+import Classes from "./Copyright.module.sass"
+
+export function Copyright() {
+    return (
+        <p className={Classes.copyright}>
+            Copyright, SportSee 2020
+        </p>
+    );
+}
